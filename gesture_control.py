792aaa447b07d3283ab4841a4f8835ee7f4858b6
Alex Pereira <alex.pereira.6464@gmail.com>
2022-10-19 23:53:09 -0400

    cvZone Determined that cvzone is simple enough that I can remake their code

--- gesture_control.py
+++ gesture_control.py
@@ -3,12 +3,15 @@
 # Imports
 import numpy as np
 import cv2   as cv
-import serial_comms
-import time
+import serial
+
+# This will be removed soon as I would like to use my own code
+from cvzone.HandTrackingModule import HandDetector
+detector = HandDetector(maxHands = 1, detectionCon = 0.8)
 
 # Creates the Gesture Class
 class Gesture:
     # Constructor
     def __init__(self) -> None:
         # Create the serial device
-        self.arduino = serial_comms.Serial('COM4', 9600)
+        self.arduino = serial.Serial('COM4', 9600)
